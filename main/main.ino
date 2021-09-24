f63bf17e089b3145c2211883c15174c981774fa3
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-24 09:53:40 +0200

    J2

--- main/main.ino
+++ main/main.ino
@@ -4,21 +4,28 @@ int BUTTON = 7;
 int SERVO = 4;
 int POT = 8;
 Servo servo;
+int buttonState;
+int oldbuttonState;
 
 void setup() {
   
-  pinMod(POT, INPUT);
+  pinMode(POT, INPUT);
   pinMode(BUTTON, INPUT);
   servo.attach(SERVO);
-  
+  Serial.begin(9600);
+
 }
 
 void loop() {
   
-  if (digitalRead(BUTTON) == HIGH) {
-    servo.write();
-  } else {
-    servo.write(0);
-  }
-
+   buttonState = digitalRead(BUTTON);
+   if (oldbuttonState != buttonState && buttonState == HIGH)
+   {
+      int power = map(analogRead(POT), 0 , 1023, 0, 180);
+      Serial.println(power);
+      servo.write(power);
+      delay(2000);
+      servo.write(0);
+   }
+   oldbuttonState = buttonState;
 }
