22b1a0c22b07be854a4c8658a8e82e74bcd8b9fb
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-09-30 22:10:50 +0200

    Push everything

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,11 @@
 #include <Servo.h>
 
 int BUTTON = 7;
-int SERVO = 4;
+int SERVO1 = 4;
+int SERVO2 = 3;
 int POT = 8;
-Servo servo;
+Servo servo1;
+Servo servo2;
 int buttonState;
 int oldbuttonState;
 
@@ -11,7 +13,8 @@ void setup() {
   
   pinMode(POT, INPUT);
   pinMode(BUTTON, INPUT);
-  servo.attach(SERVO);
+  servo1.attach(SERVO1);
+  servo2.attach(SERVO2);
   Serial.begin(9600);
 
 }
@@ -21,11 +24,25 @@ void loop() {
    buttonState = digitalRead(BUTTON);
    if (oldbuttonState != buttonState && buttonState == HIGH)
    {
-      int power = map(analogRead(POT), 0 , 1023, 0, 180);
-      Serial.println(power);
-      servo.write(power);
+      Serial.println("Démarrage de la séquence de tir...");
       delay(2000);
-      servo.write(0);
+      
+      Serial.println("Tension de l'élastique...");
+      servo1.write(180);
+      delay(2000);
+      Serial.println("Ouverture de la catapulte...");
+      servo2.write(180);
+
+      delay(5000);
+      Serial.println("Désarmement de l'élastique...");
+      servo1.write(0);
+      delay(3000);
+      Serial.println("Fermeture de la catapulte...");
+      servo2.write(0);
+
+      delay(2000);
+      Serial.println("Séquence de tir terminée !");
    }
    oldbuttonState = buttonState;
+   
 }
