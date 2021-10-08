47087e9843523d25b57c1efb61d069071c5cd097
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-10-08 09:52:41 +0200

    Update

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,15 @@
 #include <Servo.h>
+#include "rgb_lcd.h"
 
 int BUTTON = 7;
 int SERVO1 = 4;
 int SERVO2 = 3;
-int POT = 8;
+int POT = 0;
 Servo servo1;
 Servo servo2;
 int buttonState;
 int oldbuttonState;
+rgb_lcd LCD;
 
 void setup() {
   
@@ -17,6 +19,11 @@ void setup() {
   servo2.attach(SERVO2);
   Serial.begin(9600);
 
+  LCD.begin(16, 2);
+
+  LCD.setRGB(0, 255, 0);
+  afficherLCD("Test !");
+
 }
 
 void loop() {
@@ -24,25 +31,43 @@ void loop() {
    buttonState = digitalRead(BUTTON);
    if (oldbuttonState != buttonState && buttonState == HIGH)
    {
-      Serial.println("Démarrage de la séquence de tir...");
+      afficherLCD("Démarrage de la séquence de tir...");
       delay(2000);
       
-      Serial.println("Tension de l'élastique...");
+      afficherLCD("Tension de l'élastique...");
       servo1.write(180);
       delay(2000);
-      Serial.println("Ouverture de la catapulte...");
+      afficherLCD("Ouverture de la catapulte...");
       servo2.write(180);
 
       delay(5000);
-      Serial.println("Désarmement de l'élastique...");
+      afficherLCD("Désarmement de l'élastique...");
       servo1.write(0);
       delay(3000);
-      Serial.println("Fermeture de la catapulte...");
+      afficherLCD("Fermeture de la catapulte...");
       servo2.write(0);
 
       delay(2000);
-      Serial.println("Séquence de tir terminée !");
+      afficherLCD("Séquence de tir terminée !");
    }
    oldbuttonState = buttonState;
    
+}
+
+void afficherLCD(String text) {
+  if (text.length < 32) {
+    String line1 = text.slice(
+  } else {
+    throw "Cannot display this."
+  }
+  
+  LCD.setCursor(0, 0);
+  LCD.print(text);
+  
+}
+
+void sliceInTwo(String text) {
+
+  List<String>
+
 }
