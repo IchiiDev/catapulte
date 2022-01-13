36b8bd726b9b0bb6660727eb2f36b89e00d76e54
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2022-01-13 15:02:41 +0100

    Adding code doc

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,6 @@
 
 int BUTTON1 = 7;
 int BUTTON2 = 2;
-int COMPTEUR = 5;
 int SERVO1 = 4;
 int SERVO2 = 3;
 int SWITCH = 8;
@@ -14,13 +13,15 @@ int buttonState;
 int oldbuttonState;
 rgb_lcd LCD;
 
+/*
+ Fonction de démarrage
+*/
 void setup() {
   
   pinMode(POT, INPUT);
   pinMode(BUTTON1, INPUT);
   pinMode(BUTTON2, INPUT);
   pinMode(SWITCH, INPUT);
-  pinMode(COMPTEUR, OUTPUT);
   servo1.attach(SERVO1);
   servo2.attach(SERVO2);
   servo1.write(0);
@@ -34,31 +35,49 @@ void setup() {
 }
 
 void loop() {
- 
+   
+   // Mode de tir via controle IHM
    if (digitalRead(SWITCH) == HIGH) {
-     delay(150);
+     delay(150); 
+     
+     /*
+     	La carte récupère la valeur du
+     	potentiometre et défini la 
+     	rotation du servomoteur 1
+     	(elastique) à cette valeur */
      int potValue = analogRead(POT);
      int power = map(potValue, 0, 1023, 0, 180);
      servo1.write(power);
-     String str = Serial.readStringUntil('\n');
+     
+     String str = Serial.readStringUntil('\n'); // Lecture du port série géré par Python
+     // Si la commande envoyée par Python est "on", lancer le tir
      if (str == "on") {
 
-        servo2.write(180);
+        servo2.write(180); // Ouverture de la catapulte
 
         delay(3000);
-        servo1.write(0);
+        servo1.write(0); // L'élastique se détend
 
         delay(3000);
-        servo2.write(90);
+        servo2.write(90); // Fermeture de la catapulte
         
      }
    
+   // Mode de tir par séquence
    } else {
      setup();
     
+     /*
+     	Ne s'effectue qu'une fois le bouton pressé
+     	*/
      buttonState = digitalRead(BUTTON1);
      if (oldbuttonState != buttonState && buttonState == HIGH)
      {
+     	/*
+     	La carte récupère la valeur du
+     	potentiometre et défini la 
+     	rotation du servomoteur 1
+     	(elastique) à cette valeur */
         int potValue = analogRead(POT);
         int power = map(potValue, 0, 1023, 0, 180);
         afficherLCD("Demarrage", "sequence de tir");
@@ -86,6 +105,12 @@ void loop() {
    
 }
 
+
+/*
+ Paramètres: {text1} {text2} -> String
+ Les chaines de caractère à afficher 
+ (ligne par ligne)
+*/
 void afficherLCD(String text1, String text2) {
   if (text1.length() < 16 && text2.length() < 16) {
      LCD.clear();
